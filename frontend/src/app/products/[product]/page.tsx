03022e8bdfa476d4b51ef17b2c7b43a72e986cfb
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-12-16 14:17:05 +0000

    Show parameter in title of the page

--- frontend/src/app/products/[product]/page.tsx
+++ frontend/src/app/products/[product]/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function ProductsHomePage({params}: { params: { product: string } }) {
     return (
         <>
-            <h1 className="text-6xl font-bold text-slate-900">{params.product}</h1>
+            <h1 className="text-6xl font-bold text-slate-900">Products - {params.product}</h1>
             <hr className="w-5/12 h-1 bg-gray-200 rounded"></hr>
         </>
     );

f8df0a4e9ceb0614402fe49e62b4146e0966a1e4
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-12-18 18:14:01 +0000

    Make stats card content different to activity card content

--- frontend/src/app/dashboard/@stats/page.tsx
+++ frontend/src/app/dashboard/@stats/page.tsx
@@ -4,12 +4,8 @@ import Card from "@/components/Card";
 export default function Stats() {
     return (
         <Card title={"Stats"}>
-            <p>Last Activity: 2 Days ago</p>
-            <p>Recent Activities:</p>
-            <ul className={"list-disc px-4"}>
-                <li>You posted 'How to make an app...' 2 days ago.</li>
-                <li>You added Ash as a friend last week.</li>
-            </ul>
+            <p>Total friends: 16</p>
+            <p>Total Conversations Started: 7</p>
         </Card>
     );
 }

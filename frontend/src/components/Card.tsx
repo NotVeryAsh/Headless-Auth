32289fc89c8fd058beac5f8dbaa8008a387baa5e
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-12-18 18:14:45 +0000

    Make card easier to see with their colors

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Card({title, children}: { title?: string, children?: React.ReactNode }) {
     return (
-        <div className={"bg-sky-50 flex flex-col rounded-2xl drop-shadow-sm"}>
+        <div className={"bg-sky-100 flex flex-col rounded-2xl drop-shadow-sm"}>
             {title ?
-                <h2 className={"bg-sky-100 p-5 text-xl font-medium rounded-t-2xl"}>{title}</h2>
+                <h2 className={"bg-sky-200 p-5 text-xl font-medium rounded-t-2xl"}>{title}</h2>
                 : null
             }
             <div className={"p-4 space-y-3"}>

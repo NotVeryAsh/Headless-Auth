275a05cccb6bcfbdb98d22aaaa00af9264646d82
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-12-28 23:35:39 +0000

    Add back button for all pages

---
+++ frontend/src/app/@backbutton/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from "react";
+import {useRouter} from 'next/navigation'
+import Button from "@/components/Button";
+import { usePathname } from 'next/navigation'
+
+export default function Backbutton() {
+
+    const router = useRouter();
+    const pathName = usePathname();
+
+    const hidden = pathName === '/' ? 'hidden': '';
+    const classNames = hidden + ' mr-auto';
+
+    return (
+        <Button onClick={() => router.back()} classNames={classNames}>Back</Button>
+    );
+}
--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,15 +1,14 @@
 'use client'
 
-import React, {ReactNode} from "react";
-import { useFormStatus } from "react-dom";
+import React, {MouseEventHandler, ReactNode} from "react";
 
-export default function Button({disabled=false, children}: {disabled: boolean, children?: ReactNode}) {
+export default function Button({disabled=false, children, onClick, classNames=""}: {disabled?: boolean, children?: ReactNode, onClick?: MouseEventHandler, classNames?: string}) {
 
-    const ClassNames = "bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" +
+    const ClassNames = classNames + " bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" +
         (disabled ? " opacity-50 cursor-not-allowed" : "")
 
     return (
-        <button className={ClassNames} aria-disabled={disabled}>
+        <button className={ClassNames} aria-disabled={disabled} onClick={onClick}>
             {children}
         </button>
     );

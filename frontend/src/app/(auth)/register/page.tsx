4fbe14faa3fd0d43c060ca5ee0f93740e2ce4730
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2024-02-14 22:15:31 +0000

    adjust input placeholder for name on register page

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -14,7 +14,7 @@ function Register() {
         <h1 className="large-title">Register</h1>
         <hr className="w-3/12 h-1 bg-gray-200 rounded"></hr>
             <AuthenticationForm method={"POST"} action={"/api/auth/register"} buttonText={"Register"}>
-                <input required={true} type="text" name="name" placeholder="Email" className="input" maxLength={255} />
+                <input required={true} type="text" name="name" placeholder="Name" className="input" maxLength={255} />
                 <input required={true} type="email" name="email" placeholder="Email" className="input" maxLength={255} />
                 <input required={true} type="password" name="password" placeholder="Password" className="input" />
                 <input required={true} type="password" name="password_confirmation" placeholder="Password Confirmation" className="input" />
